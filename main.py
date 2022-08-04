61b74c13ea2ec2c9925f6a584b9f60a2e5fb683e
baidoolot <bdtbaidoolot@gmail.com>
2022-08-04 18:24:23 +0600

    heroku login

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from database1.bot_db import sql_create
 async def on_startup(_):
     asyncio.create_task(notification.scheduler())
     sql_create()
+
 extra.register_handlers_extra(dp)
 fsmAdminMenu.register_handlers_fsmAdminMenu(dp)
 client.register_handlers_client(dp)
@@ -15,6 +16,7 @@ fsmAdminMenu.register_handlers_fsmAdminMenu(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
 news.register_handlers_news(dp)
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
